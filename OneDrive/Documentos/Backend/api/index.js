ef29109ca42b57857cdd3a0269fa312e380c9c45
btxone <btxpablo@gmail.com>
2023-01-14 13:43:45 -0300

    sas

---
+++ OneDrive/Documentos/Backend/api/index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const server = require('./src/app')
+
+const dbConnect = require('./src/db')
+
+const morgan = require('morgan')
+
+server.use(morgan("dev"));
+
+// port va a buscar un puerto que nos de el servicio de hosting donde deployamos, si no, toma el que le pasemos
+const port = process.env.PORT || 9000;
+
+
+server.listen( port, () => {
+    console.log(`listening on port ${port}`)
+});
+
+dbConnect();
