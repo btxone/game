52d3bc0fe1f56b195c4723f5eaf40ca0f134dffe
btxone <97922730+btxone@users.noreply.github.com>
2023-02-27 12:31:00 -0300

    Delete OneDrive directory

--- OneDrive/Documentos/Backend/api/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const models = {
-    productModel: require('./product.js'),
-    categoryModel: require('./category.js'),
-    userModel: require('./user.js'),
-    reviewsModel: require('./reviews.js'),
-    buggyModel: require('./buggy.js'),
-}
-
-module.exports = models;
